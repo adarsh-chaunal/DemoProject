042fdd19004fe68d3d08acd73a45af3469598009
Adarsh Chaunal <er.adarsh97@gmail.com>
2023-08-11 13:24:06 +0530

    #

--- DemoProject/Controllers/SettingController.cs
+++ DemoProject/Controllers/SettingController.cs
@@ -40,6 +40,65 @@ namespace DemoProject.Controllers
 			return View("Index");
 		}
 
-	}
+        public IActionResult Edit(int? id) 
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            Setting? settingFromDb = _db.Settings.FirstOrDefault(u => u.Id == id); 
+
+            if (settingFromDb == null)
+            {
+                return NotFound();
+            }
+            return View(settingFromDb);
+        }
+        [HttpPost]
+        public IActionResult Edit(Setting setting)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Settings.Update(setting);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+
+        public IActionResult Delete(long? id)
+        {
+            //var setting = _db.Settings.Find(id);
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            Setting? settingFromDb = _db.Settings.Find(id); // only works on primary key
+
+            if (settingFromDb == null)
+            {
+                return NotFound();
+            }
+            return View(settingFromDb);
+
+            //_db.Settings.Remove(setting);
+            //_db.SaveChanges();
+
+            //return RedirectToAction("Index");
+        }
+        [HttpPost, ActionName("Delete")] // action name is delete even ve have to give it an another name 
+        public IActionResult DeletePOST(long? id)
+        {
+            Setting? setting = _db.Settings.Find(id);
+            if (setting == null)
+            {
+                return NotFound();
+            }
+            _db.Settings.Remove(setting);
+            _db.SaveChanges();
+            
+            return RedirectToAction("Index");
+        }
+    }
 }
 
